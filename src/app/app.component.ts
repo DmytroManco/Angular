8b99d20bf9c013a3e286e1003d2ceace5722aaa4
Dmytro Putrin <moncodp@gmail.com>
2018-12-05 15:13:56 +0200

    [Master] clean master branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,48 +1,18 @@
-import { AfterViewInit, AfterContentInit, Component, ComponentFactoryResolver, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
-
-import { AuthComponent } from './auth/auth.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, AfterContentInit {
-  subject: BehaviorSubject<number> = new BehaviorSubject(NaN);
-  stream1$: Observable<number>;
-  stream2$: Observable<number>;
+export class AppComponent implements OnInit {
 
-  @ViewChild('entry', {read: ViewContainerRef}) entry: ViewContainerRef;
 
-  constructor(private resolver: ComponentFactoryResolver) {
-  }
-
-  auth(user) {
-    console.log(user);
+  constructor() {
   }
 
   ngOnInit() {
-    this.subject.next(1);
-    this.stream1$ = this.subject.asObservable();
-    this.stream2$ = this.subject.asObservable();
   }
 
-  ngAfterContentInit() {
-    const authFactory = this.resolver.resolveComponentFactory(AuthComponent);
-    const component = this.entry.createComponent(authFactory);
-  }
-
-  logStream(stream: Observable<number>) {
-    stream.subscribe(console.log);
-  }
-
-  changeValue() {
-    const random = Math.random();
-    this.subject.next(random);
-  }
-
-  rememberUser(checked: boolean) {
-    console.log('App Component', checked);
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AuthComponent } from './auth/auth.component';
 import { FormsModule } from '@angular/forms';
-import { AuthRememberComponent } from './auth-remember/auth-remember.component';
-import { AuthMessageComponent } from './auth/auth.message';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent,
-    AuthRememberComponent,
-    AuthMessageComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
   ],
   providers: [],
-  entryComponents: [
-    AuthComponent,
-  ],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
