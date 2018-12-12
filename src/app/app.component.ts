d67be228e99231635125df7667dccd356e688e5c
Dmytro Putrin <moncodp@gmail.com>
2018-12-12 09:50:47 +0200

    [Combination operators]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Subject, zip, combineLatest, forkJoin, withLatestFrom } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -7,12 +8,33 @@ import { BehaviorSubject, Observable } from 'rxjs';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-
+  color$ = new Subject<Color>();
+  logo$ = new Subject<Logo>();
 
   constructor() {
   }
 
   ngOnInit() {
+    // zip(this.color$, this.logo$).subscribe(([color, logo]) => console.log(`${color} shit with ${logo}`));
+    // combineLatest(this.color$, this.logo$).subscribe(([color, logo]) => console.log(`${color} shit with ${logo}`));
+    // this.color$.pipe(
+    //   withLatestFrom(this.logo$)
+    // ).subscribe(([color, logo]) => console.log(`${color} shit with ${logo}`));
+    forkJoin(this.color$, this.logo$).subscribe(([color, logo]) => console.log(`${color} shit with ${logo}`));
+
+    this.color$.next('white');
+    this.logo$.next('fish');
+    this.color$.next('green');
+    this.logo$.next('dog');
+    this.color$.next('red');
+    this.logo$.next('bird');
+    this.color$.next('blue');
+
+    this.color$.complete();
+    this.logo$.complete();
   }
 
 }
+
+type Color = 'white' | 'green' | 'red' | 'blue';
+type Logo = 'fish' | 'dog' | 'bird' | 'cow';
